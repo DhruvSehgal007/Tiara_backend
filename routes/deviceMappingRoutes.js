b3046118cbdacc6f33b76820f80c7f13973410dd
homan sharma <hs8146400@gmail.com>
2025-11-08 13:06:37 +0530

    delete extra files

--- routes/deviceMappingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const deviceMappingController = require("../controllers/deviceMappingController");
-const { verifyToken } = require("../middleware/authMiddleware");
-
-router.post("/save", verifyToken, deviceMappingController.saveDeviceMapping);
-router.get("/get/:device_name", verifyToken, deviceMappingController.getDeviceMapping);
-
-module.exports = router;
