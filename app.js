eb64e6d97cddb408417c08236f73ee9b22fcbaf0
homan sharma <hs8146400@gmail.com>
2025-11-10 16:13:11 +0530

    aad console

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const session = require('express-session');
 
 dotenv.config();
 
@@ -18,3 +19,10 @@ app.use("/api/", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // secure:true when using HTTPS
+}));
